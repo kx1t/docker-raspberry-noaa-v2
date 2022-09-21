ba7cc03b7b21b6170c3d449c7456f5b80b5114bb
kx1t <ramonkolb@gmail.com>
2022-09-20 20:31:04 -0400

    bug fix

--- rootfs/RaspiNOAA2/scripts/reload_nginx.sh
+++ rootfs/RaspiNOAA2/scripts/reload_nginx.sh
@@ -4,10 +4,10 @@
 APPNAME="$(hostname)/reload_nginx"
 
 set -a
-if [[ -f /run/nginx/pid ]]
+if [[ -f /run/nginx.pid ]]
 then
     echo "[$(date)][$APPNAME] Reloading NGINX ..."
-    kill -HUP "$(cat /run/nginx/pid)"
+    kill -HUP "$(cat /run/nginx.pid)"
 else
     echo "[$(date)][$APPNAME] NGINX is not (yet) running. Skipping Reload!"
 fi
