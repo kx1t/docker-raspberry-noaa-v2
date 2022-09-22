61c0e14edddc2737e959202c7c53a71a186ef941
kx1t <ramonkolb@gmail.com>
2022-09-22 11:56:40 -0400

    updates to webpages

--- rootfs/RaspiNOAA2/scripts/reload_webpanel.sh
+++ rootfs/RaspiNOAA2/scripts/reload_webpanel.sh
@@ -3,10 +3,14 @@
 
 APPNAME="$(hostname)/reload_nginx"
 
+# import common lib and settings
+source "$NOAA_HOME/.noaa-v2.conf"
+source "$NOAA_HOME/scripts/common.sh"
+
 set -a
 (
   find $WEB_HOME/ -mindepth 1 -type d -name "Config" -prune -o -print | xargs rm -rf &&
   cp -r $NOAA_HOME/webpanel/* $WEB_HOME/ &&
   chown -R root:www-data $WEB_HOME/ &&
   composer install -d $WEB_HOME/
-) || echo "  Something went wrong updating web content - please inspect the logs above"
+) || echo "[$(date)][$APPNAME] Something went wrong updating web content - please inspect the logs above"
