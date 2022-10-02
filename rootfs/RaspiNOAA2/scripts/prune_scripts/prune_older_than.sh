13527840b80d98cbaeec475b67f0a3d74ef3eac8
kx1t <ramonkolb@gmail.com>
2022-10-02 13:52:26 -0400

    added pruning of audio files

--- rootfs/RaspiNOAA2/scripts/prune_scripts/prune_older_than.sh
+++ rootfs/RaspiNOAA2/scripts/prune_scripts/prune_older_than.sh
@@ -12,9 +12,14 @@ let prunedate=$(date +%s)-$PRUNE_OLDER_THAN*24*60*60
 
 log "Pruning captures..." "INFO"
 for img_path in $(sqlite3 ${DB_FILE} "select file_path from decoded_passes where pass_start < $prunedate;"); do
-    find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {};
-    find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {};
+    find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name '${img_path}*.jpg' -exec rm -f {} \;
+    find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name '${img_path}*.jpg' -exec rm -f {} \;
     log "  ${img_path} file pruned" "INFO"
     sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
     log "  Database entry pruned" "INFO"
 done
+
+log "Pruning recordings, if any..." "INFO"
+find "${NOAA_AUDIO_OUTPUT}"/*  -maxdepth 1 -mtime +$PRUNE_OLDER_THAN -exec rm {} \;
+find "${METEOR_AUDIO_OUTPUT}"/*  -maxdepth 1 -mtime +$PRUNE_OLDER_THAN -exec rm {} \;
+log "Done pruning recordings" "INFO"
