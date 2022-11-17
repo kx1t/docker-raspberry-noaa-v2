730157f06e1e64818dc882f6fc588ddbd2121561
kx1t <ramonkolb@gmail.com>
2022-11-17 17:13:49 -0500

    switch bacl to meteor_demod

--- rootfs/RaspiNOAA2/scripts/audio_processors/meteor_demodulate_qpsk.sh
+++ rootfs/RaspiNOAA2/scripts/audio_processors/meteor_demodulate_qpsk.sh
@@ -20,13 +20,13 @@ OUTPUT_QPSK=$2
 
 
 # produce the output image - note QPSK output lists before wav
-# $METEOR_DEMOD -B -o "${OUTPUT_QPSK}" "${INPUT_WAV}" >> $NOAA_LOG 2>&1
+$METEOR_DEMOD -B -o "${OUTPUT_QPSK}" "${INPUT_WAV}" >> "$NOAA_LOG" 2>&1
 # note we switched to DigitElektro's MeteorDemod (https://github.com/Digitelektro/MeteorDemod)
 # Also reduced the amount of log messages a bit -- only logged when signal is locked, or at 5/10/15/etc %
-$METEOR_DEMOD -i "${INPUT_WAV}" -o "${OUTPUT_QPSK}" -t "${TLE_OUTPUT}" -f jpg 2> "${NOAA_LOG}" \
-    | stdbuf -oL tr '\r' '\n' | grep "Progress: [0-9]*[05].00\|isLocked: 1\|synch:\|Decoded\|received" >> "${NOAA_LOG}"
-
-if ! grep "No data received" <<< "$(tail "${NOAA_LOG}")"
-then
-    exit 1
-fi
+# $METEOR_DEMOD -i "${INPUT_WAV}" -o "${OUTPUT_QPSK}" -t "${TLE_OUTPUT}" -f jpg 2> "${NOAA_LOG}" \
+#     | stdbuf -oL tr '\r' '\n' | grep "Progress: [0-9]*[05].00\|isLocked: 1\|synch:\|Decoded\|received" >> "${NOAA_LOG}"
+#
+# if ! grep "No data received" <<< "$(tail "${NOAA_LOG}")"
+# then
+#     exit 1
+# fi
